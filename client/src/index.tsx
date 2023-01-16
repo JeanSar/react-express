08c13dcee1a15152bf6df7517c6aa93c18ee8d63
JeanSar <jeansar504@gmail.com>
2023-01-16 17:01:33 +0100

    init client 2

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+// Import de l'image
+import LOGO from "./static/logo.jpg";
 
 const Index = () => {
-  return <div>TIW 8 TP1!</div>;
+  return <div>TIW 8 TP1!<img src={LOGO} alt="Logo" /></div>;
 };
 ReactDOM.render(<Index />, document.getElementById("root"));
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,8 +4,17 @@ import { HelloRouteur } from './routes/hello.router';
 const app = express();
 const port = process.env.PORT || 3000;
 
+const path = require("path");
+
+const DIST_DIR = path.join(__dirname, "../../client/dist");
+const HTML_FILE = path.join(DIST_DIR, "index.html");
+
 app.listen(port, () => {
   process.stdout.write(`Server started on port: ${port}\n`);
 });
 
+app.use(express.static(DIST_DIR));
+app.get('/', (req, res) => {
+  res.sendFile(HTML_FILE)
+})
 app.use('/hello', HelloRouteur);
