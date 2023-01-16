1d8fc6d85730f07ca640a33546bda38a93a0e59d
JeanSar <jeansar504@gmail.com>
2023-01-16 15:28:27 +0100

    init client 1

---
+++ client/src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+const Index = () => {
+  return <div>TIW 8 TP1!</div>;
+};
+ReactDOM.render(<Index />, document.getElementById("root"));
