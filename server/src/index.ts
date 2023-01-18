56f73acd81015d557a634f0158043125d981999b
JeanSar <jeansar504@gmail.com>
2023-01-18 15:52:25 +0100

    Eslint ok sur tout le projet

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,20 @@
-import express from 'express';
-import { HelloRouteur } from './routes/hello.router';
+import express from 'express'
+import { HelloRouteur } from './routes/hello.router'
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT ?? 3000
 
-const path = require("path");
+const path = require('path')
 
-const DIST_DIR = path.join(__dirname, "../../client/dist");
-const HTML_FILE = path.join(DIST_DIR, "index.html");
+const DIST_DIR = path.join(__dirname, '../../client/dist')
+const HTML_FILE = path.join(DIST_DIR, 'index.html')
 
 app.listen(port, () => {
-  process.stdout.write(`Server started on port: ${port}\n`);
-});
+  process.stdout.write(`Server started on port: ${port}\n`)
+})
 
-app.use(express.static(DIST_DIR));
+app.use(express.static(DIST_DIR))
 app.get('/', (req, res) => {
   res.sendFile(HTML_FILE)
 })
-app.use('/hello', HelloRouteur);
+app.use('/hello', HelloRouteur)
